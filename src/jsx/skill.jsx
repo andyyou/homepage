e098dd6edd5a0f4d4e351e29cffd1dbefd8cac13
이재성 <jason0853@gmail.com>
2015-11-08 22:16:28 +0900

    Bar chart is completed.

--- src/jsx/skill.jsx
+++ src/jsx/skill.jsx
@@ -1,4 +1,4 @@
-var React = require('react');
+ var React = require('react');
 var Well = require('react-bootstrap').Well;
 var $ = require('jquery');
 var d3 = require('d3');
@@ -17,7 +17,7 @@ var skill = React.createClass({
 			    			<div id="wordCloud"></div>
 			    		</div>
 			    		<div className="skill_part">
-			    			<div id="chart">
+			    			<div id="barChart">
 
 			    			</div>
 			    		</div>
@@ -50,7 +50,7 @@ var skill = React.createClass({
 					     		      .attr('transform', 'translate(' + wordCloudWidth / 2 + ', ' +  wordCloudHeight / 2 + ')');
 			
 			// json file data mapping	      
-			d3.json("data/data.json", function (data) {
+			d3.json("data/wordCloud.json", function (data) {
 
 				var lang = data.map(function (d) { return { text: d.language, size: 15 + Math.random() * 50 } });
 
@@ -106,7 +106,7 @@ var skill = React.createClass({
 		setInterval(function (){
 			d3.select('#wordCloud').select('svg').remove();
 			drawWordCloud(wordCloudWidth, wordCloudHeight);
-		}, 10000);
+		}, 8000);
 
 	     	// resizeDrawWordCloud 함수 정의
 	     	function resizeDrawWordCloud() {
@@ -114,7 +114,7 @@ var skill = React.createClass({
 			drawWordCloud(wordCloudWidth, wordCloudHeight);
 		}
 
-		// 해상도에 따라 resizeDrawWordCloud 함수 호
+		// 해상도에 따라 resizeDrawWordCloud 함수 호출
 		d3.select(window).on('resize', resizeDrawWordCloud); 
 
 		/***************************************************************
@@ -122,9 +122,124 @@ var skill = React.createClass({
 		* D3.js Bar Chart setting
 		*
 		***************************************************************/
+		var margin = { top: 10, right: 10, bottom: 60, left: 40},
+			barChartWidth = $('#barChart').width() - margin.right - margin.left;
+			barChartHeight = $('#barChart').height() - margin.top - margin.bottom;
+
+		// drawBarChart 함수 정의
+		function drawBarChart (barChartWidth, barChartHeight) {
+			var margin = { top: 10, right: 10, bottom: 60, left: 40},
+			barChartWidth = $('#barChart').width() - margin.right - margin.left;
+			barChartHeight = $('#barChart').height() - margin.top - margin.bottom;
+
+			// Define canvas 
+			var canvas = d3.select('#barChart')
+								.append('svg')
+									.attr({
+										'width' : barChartWidth + margin.right + margin.left,
+										'height' : barChartHeight + margin.top + margin.bottom
+									})
+								.append('g')
+									.attr('transform', 'translate(' + margin.left + ', ' + margin.right + ')');
+
+			// Define the x y scales
+			var xScale = d3.scale.ordinal()
+							.rangeRoundBands([0, barChartWidth], 0.2, 0.2);
+
+			var yScale = d3.scale.linear()
+							.range([barChartHeight, 0]);
+
+			// Define axis
+			var xAxis = d3.svg.axis()
+						.scale(xScale)
+						.orient('bottom');
+
+			var yAxis = d3.svg.axis()
+						.scale(yScale)
+						.orient('left')
+						.tickFormat(function (d) {
+							return d + '%';
+						});
+
+			// barChart data mapping
+			d3.json("data/barChart.json", function (error, data) {
+
+				if (error) console.log('Error: data not loaded');
+
+				data.forEach(function (d) {
+					d.percent = +d.percent;
+					d.language = d.language;
+
+					console.log(d.percent);
+				});
+
+				data.sort(function (a, b) {
+					return b.percent - a.percent;
+				});
+
+				xScale.domain(data.map(function (d) { return d.language; }) );
+				yScale.domain([0, 100]);
+
+				canvas.selectAll('rect')
+					.data(data)
+					.enter()
+					.append('rect')
+					.attr('height', 0)
+					.attr('y', barChartHeight)
+					.attr('rx', '15px')
+					.transition().duration(3000)
+					.delay(function (d, i) { return i * 200 })
+					.attr({
+						'x': function (d) { return xScale(d.language); },
+						'y': function (d) { return yScale(d.percent); },
+						'width': xScale.rangeBand(),
+						'height': function (d) { return barChartHeight - yScale(d.percent); }
+					})
+					.style('fill', function (d, i) { return 'rgb(46, 215, ' + ((i * 30) + 100) + ')' });
+
+				canvas.selectAll('text')
+						.data(data)
+						.enter()
+						.append('text')
+						.text(function(d) { return d.percent; })
+						.attr('x', function(d) { return xScale(d.language) + xScale.rangeBand() / 2; })
+						.attr('y', function(d) { return yScale(d.percent) + 17; })
+						.style('fill', 'white')
+						.style('text-anchor', 'middle');
 
 
+				canvas.append('g')
+						.attr('class', 'x axis')
+						.attr('transform', 'translate(0, ' + barChartHeight + ')')
+						.call(xAxis)
+						.selectAll('text')
+						.attr('transform', 'rotate(-60)')
+						.attr('dx', '-.8em')
+						.attr('dy', '-.25em')
+						.style('fill', '#F7B9D9')
+						.style('text-anchor', 'end');
 
+				canvas.append('g')
+						.attr('class', 'y axis')
+						.call(yAxis)
+						.style('fill', '#F7B9D9');
+
+			});
+		}
+
+		// drawBarChart 호출 after dom is loaded
+		drawBarChart(barChartWidth, barChartHeight);
+
+		// resizeDrawWordCloud 함수 정의
+	     	function resizeDrawBarChart() {
+	     		d3.select('#barChart').select('svg').remove();
+			drawBarChart(barChartWidth, barChartHeight);
+		}
+
+		// 해상도에 따라 resizeDrawBarChart 함수 호출
+		d3.select(window).on('resize', resizeDrawBarChart); 
+
+		
 
 
 		// var $lang = $('.skill_lang div');
--- test/script/app.js
+++ test/script/app.js
@@ -53932,7 +53932,7 @@ var logo = React.createClass({displayName: "logo",
 
 module.exports = logo;
 },{"react":390}],395:[function(require,module,exports){
-var React = require('react');
+ var React = require('react');
 var Well = require('react-bootstrap').Well;
 var $ = require('jquery');
 var d3 = require('d3');
@@ -53951,7 +53951,7 @@ var skill = React.createClass({displayName: "skill",
 			    			React.createElement("div", {id: "wordCloud"})
 			    		), 
 			    		React.createElement("div", {className: "skill_part"}, 
-			    			React.createElement("div", {id: "chart"}
+			    			React.createElement("div", {id: "barChart"}
 
 			    			)
 			    		)
@@ -53984,7 +53984,7 @@ var skill = React.createClass({displayName: "skill",
 					     		      .attr('transform', 'translate(' + wordCloudWidth / 2 + ', ' +  wordCloudHeight / 2 + ')');
 			
 			// json file data mapping	      
-			d3.json("data/data.json", function (data) {
+			d3.json("data/wordCloud.json", function (data) {
 
 				var lang = data.map(function (d) { return { text: d.language, size: 15 + Math.random() * 50 } });
 
@@ -54040,7 +54040,7 @@ var skill = React.createClass({displayName: "skill",
 		setInterval(function (){
 			d3.select('#wordCloud').select('svg').remove();
 			drawWordCloud(wordCloudWidth, wordCloudHeight);
-		}, 10000);
+		}, 8000);
 
 	     	// resizeDrawWordCloud 함수 정의
 	     	function resizeDrawWordCloud() {
@@ -54048,7 +54048,7 @@ var skill = React.createClass({displayName: "skill",
 			drawWordCloud(wordCloudWidth, wordCloudHeight);
 		}
 
-		// 해상도에 따라 resizeDrawWordCloud 함수 호
+		// 해상도에 따라 resizeDrawWordCloud 함수 호출
 		d3.select(window).on('resize', resizeDrawWordCloud); 
 
 		/***************************************************************
@@ -54056,9 +54056,124 @@ var skill = React.createClass({displayName: "skill",
 		* D3.js Bar Chart setting
 		*
 		***************************************************************/
+		var margin = { top: 10, right: 10, bottom: 60, left: 40},
+			barChartWidth = $('#barChart').width() - margin.right - margin.left;
+			barChartHeight = $('#barChart').height() - margin.top - margin.bottom;
+
+		// drawBarChart 함수 정의
+		function drawBarChart (barChartWidth, barChartHeight) {
+			var margin = { top: 10, right: 10, bottom: 60, left: 40},
+			barChartWidth = $('#barChart').width() - margin.right - margin.left;
+			barChartHeight = $('#barChart').height() - margin.top - margin.bottom;
+
+			// Define canvas 
+			var canvas = d3.select('#barChart')
+								.append('svg')
+									.attr({
+										'width' : barChartWidth + margin.right + margin.left,
+										'height' : barChartHeight + margin.top + margin.bottom
+									})
+								.append('g')
+									.attr('transform', 'translate(' + margin.left + ', ' + margin.right + ')');
+
+			// Define the x y scales
+			var xScale = d3.scale.ordinal()
+							.rangeRoundBands([0, barChartWidth], 0.2, 0.2);
+
+			var yScale = d3.scale.linear()
+							.range([barChartHeight, 0]);
+
+			// Define axis
+			var xAxis = d3.svg.axis()
+						.scale(xScale)
+						.orient('bottom');
+
+			var yAxis = d3.svg.axis()
+						.scale(yScale)
+						.orient('left')
+						.tickFormat(function (d) {
+							return d + '%';
+						});
+
+			// barChart data mapping
+			d3.json("data/barChart.json", function (error, data) {
+
+				if (error) console.log('Error: data not loaded');
+
+				data.forEach(function (d) {
+					d.percent = +d.percent;
+					d.language = d.language;
+
+					console.log(d.percent);
+				});
+
+				data.sort(function (a, b) {
+					return b.percent - a.percent;
+				});
+
+				xScale.domain(data.map(function (d) { return d.language; }) );
+				yScale.domain([0, 100]);
+
+				canvas.selectAll('rect')
+					.data(data)
+					.enter()
+					.append('rect')
+					.attr('height', 0)
+					.attr('y', barChartHeight)
+					.attr('rx', '15px')
+					.transition().duration(3000)
+					.delay(function (d, i) { return i * 200 })
+					.attr({
+						'x': function (d) { return xScale(d.language); },
+						'y': function (d) { return yScale(d.percent); },
+						'width': xScale.rangeBand(),
+						'height': function (d) { return barChartHeight - yScale(d.percent); }
+					})
+					.style('fill', function (d, i) { return 'rgb(46, 215, ' + ((i * 30) + 100) + ')' });
+
+				canvas.selectAll('text')
+						.data(data)
+						.enter()
+						.append('text')
+						.text(function(d) { return d.percent; })
+						.attr('x', function(d) { return xScale(d.language) + xScale.rangeBand() / 2; })
+						.attr('y', function(d) { return yScale(d.percent) + 17; })
+						.style('fill', 'white')
+						.style('text-anchor', 'middle');
 
 
+				canvas.append('g')
+						.attr('class', 'x axis')
+						.attr('transform', 'translate(0, ' + barChartHeight + ')')
+						.call(xAxis)
+						.selectAll('text')
+						.attr('transform', 'rotate(-60)')
+						.attr('dx', '-.8em')
+						.attr('dy', '-.25em')
+						.style('fill', '#F7B9D9')
+						.style('text-anchor', 'end');
 
+				canvas.append('g')
+						.attr('class', 'y axis')
+						.call(yAxis)
+						.style('fill', '#F7B9D9');
+
+			});
+		}
+
+		// drawBarChart 호출 after dom is loaded
+		drawBarChart(barChartWidth, barChartHeight);
+
+		// resizeDrawWordCloud 함수 정의
+	     	function resizeDrawBarChart() {
+	     		d3.select('#barChart').select('svg').remove();
+			drawBarChart(barChartWidth, barChartHeight);
+		}
+
+		// 해상도에 따라 resizeDrawBarChart 함수 호출
+		d3.select(window).on('resize', resizeDrawBarChart); 
+
+		
 
 
 		// var $lang = $('.skill_lang div');
